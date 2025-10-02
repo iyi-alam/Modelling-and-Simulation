f8addae3625d7688136317adfd20d5de54b78c4b
iyi-alam <alam.samsadju786@gmail.com>
2025-10-02 12:52:03 +0530

    modified simulation driver

--- single_queue_simulation/single_queue_driver.py
+++ single_queue_simulation/single_queue_driver.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import argparse
 from single_queue_geo_d_1 import single_queue_geo_d_1
 
 
@@ -29,15 +30,15 @@ def compare_plot(injection_rate_arr, waiting_time_simulation,
     plt.savefig(save_path)
 
 
-def main():
+def main(args):
     # Parameter Initialization
-    service_time = 6 #deterministic
-    injection_rate_arr = np.arange(0.05, 0.45, 0.05)  # -> increase stop limit a little bit to include the last point
+    service_time = args.service_time #deterministic
+    injection_rate_arr = np.arange(0.05, args.max_injection_rate+0.05, 0.05)  # -> increase stop limit a little bit to include the last point
     waiting_time_simulation = np.zeros(len(injection_rate_arr), dtype=np.float32)
     average_utilization = np.zeros(len(injection_rate_arr), dtype=np.float32)
     average_occupancy = np.zeros(len(injection_rate_arr), dtype=np.float32)
     #average_residual_time = np.zeros(len(injection_rate_arr), dtype=np.float32)
-    simulation_length = 1000000
+    simulation_length = args.simulation_length
 
     waiting_time_analytical = np.zeros(len(injection_rate_arr), dtype=np.float32)
     #residual_time_analytical = np.zeros(len(injection_rate_arr), dtype=np.float32)
@@ -58,10 +59,21 @@ def main():
         waiting_time_analytical[injection_rate_idx] = 0.5*rho*(service_time-1)/(1-rho)
     
     compare_plot(injection_rate_arr, waiting_time_simulation, waiting_time_analytical, 
-                 save_folder="/data/home/samsadalam/mtech_project/modelling_simulation/single_queue_simulation/outputs",
+                 save_folder= args.save_dir,
                  save_name=f"compare_svct_{service_time}_simlen_{simulation_length}.png",
                  save_title=f"Simulatio of Geo/D/1 Queue \n Service time = {service_time}, simulation length = {simulation_length}")
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--service_time", type=float, default=2.0)
+    parser.add_argument("--simulation_length", type=int, default=1000000)
+    parser.add_argument("--save_dir", type=str, default="/outputs")
+    parser.add_argument("--max_injection_rate", type=float, default=0.45)
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    main()
+    args = get_args()
+    main(args=args)
